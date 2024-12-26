81f238ad35cee02149a15054b17c6fd20c590358
Francois Perron <fperron@gmail.com>
2024-12-26 10:40:58 -0500

    Run mains from main

---
+++ src/book/_20_web_server/src/http/server.rs
@@ -0,0 +1,47 @@
+use std::io::{BufRead, BufReader, Write};
+use std::net::{TcpListener, TcpStream};
+use std::{fs, thread};
+use std::time::Duration;
+use crate::http::request::Request;
+use crate::threads::thread_pool::ThreadPool;
+
+pub struct Server {}
+
+impl Server {
+    pub fn start(base_path: &str) {
+        let address = "127.0.0.1:7878";
+        let listener = TcpListener::bind(address).unwrap();
+        let pool = ThreadPool::new(4);
+        println!("Server running on http://{address}");
+
+        for stream in listener.incoming() {
+            let stream = stream.unwrap();
+            let base_path = base_path.to_string();
+            pool.execute(move || { Server::handle(stream, &base_path) });
+        }
+    }
+
+    fn handle(mut stream: TcpStream, base_path: &str) {
+        let buf_reader = BufReader::new(&mut stream);
+        let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+        let request = Request::from(&request_line);
+        println!("{:?}", request);
+
+        match request.url.as_str() {
+            "/" => Server::response(stream, "HTTP/1.1 200 OK", &format!("{}/index.html", base_path)),
+            "/sleep" => {
+                thread::sleep(Duration::from_secs(5));
+                Server::response(stream, "HTTP/1.1 200 OK", &format!("{}/index.html", base_path))
+            }
+            _ => Server::response(stream, "HTTP/1.1 404 NOT FOUND", "404.html"),
+        }
+    }
+
+    fn response(mut stream: TcpStream, status: &str, file: &str) {
+        let contents = fs::read_to_string(file).unwrap_or_else(|_| "file not found".to_string());
+        let response = format!("{}\r\nContent-Length: {}\r\n\r\n{}", status, contents.len(), contents);
+
+        stream.write_all(response.as_bytes()).unwrap();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::io;
+use _12_minigrep::config::Config;
+
 fn main() {
-    // learning_rust::book::_02_guessing_game::guess();
+    println!("Choose wisely");
+    println!("1. Guessing game");
+    println!("2. Minigrep");
+    println!("3. Web server");
+
+    match select() {
+        1 => learning_rust::book::_02_guessing_game::guess(),
+        2 => minigrep(),
+        3 => _20_web_server::http::server::Server::start("src/book/_20_web_server"),
+        _ => println!("Invalid selection"),
+    }
+}
+
+fn select() -> u32 {
+    let mut selection = String::new();
+
+    io::stdin()
+        .read_line(&mut selection)
+        .expect("Failed to read line");
+
+    let selection: u32 = selection.trim().parse().unwrap_or_else(|_| {
+        panic!("Please type a number");
+    });
+    selection
+}
+
+fn minigrep() {
+    let config = Config { file_path: "src/book/_12_minigrep/tests/poem.txt".to_string(), query: "to".to_string() , ignore_case: true };
+    if let Err(error) = _12_minigrep::minigrep(config) {
+        println!("Application error: {:?}", error);
+    }
 }
